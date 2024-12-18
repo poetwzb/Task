693de51f0b2ccf7038ac77e407bc4a29e98865ec
poetwzb <poet_wzb@163.com>
2024-12-18 16:14:30 +0800

    Add the description of the first task

--- myTask1.py
+++ myTask1.py
@@ -4,38 +4,39 @@ from typing import List
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
 
-        # 定义查找的四个方向
+        # Define the four directions of the search
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        # 维护一个与 board 等大的visited 数组，用于标识每个位置是否被访问过
+        # Maintain a large visited array, such as board, that identifies whether each location has been visited
         visited = set()
 
-        # 深度优先搜索
+        # depth-first search
         def dfs(i: int, j: int, idx: int) -> bool:
-            # 1. 边界条件，二维数组中的当前字符与输入的字符串中的当前字符不匹配
+            # 1. Boundary condition where the current character in the
+            #    two-dimensional array does not match the current character in the input string
             if board[i][j] != word[idx]:
                 return False
-            # 2. 在二维数组中找到了匹配的字符串
+            # 2. A matching string was found in the two-dimensional array
             if idx == len(word) - 1:
                 return True
-            # 用于标识每个位置是否被访问过
+            # Used to identify whether each location has been visited
             visited.add((i, j))
             result = False
-            # 3. 从四个方向进行深度优先搜索
+            # 3. Depth-first search from four directions
             for di, dj in directions:
                 newi, newj = i + di, j + dj
-                # 边界条件，防止数组角标越界
+                # Boundary conditions that prevent array corner markers from crossing boundaries
                 if 0 <= newi < len(board) and 0 <= newj < len(board[0]):
-                    # 判断在二位数组中该位置的元素是否被访问过
+                    # Determines whether the element at that position in the 2-bit array has been accessed
                     if (newi, newj) not in visited:
-                        # 递归调用深度优先搜索
+                        # Recursively invoke depth-first search
                         if dfs(newi, newj, idx + 1):
                             result = True
                             break
-            # 回溯
+            # Back Tracking
             visited.remove((i, j))
             return result
 
-        # 在二位数组中遍历搜索指定字符串
+        # Iterates through a 2-bit array to search for the specified string
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if dfs(i, j, 0):
